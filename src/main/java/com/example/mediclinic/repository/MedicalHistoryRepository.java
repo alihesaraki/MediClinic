6329c2e9754f66d8fa4c94359b5452c9788a5ea6
ali_he96 <alihesarakitrade@gmail.com>
2025-02-02 17:00:41 +0330

    add Repository

---
+++ src/main/java/com/example/mediclinic/repository/MedicalHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.mediclinic.repository;
+
+import com.example.mediclinic.model.MedicalHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
+}
