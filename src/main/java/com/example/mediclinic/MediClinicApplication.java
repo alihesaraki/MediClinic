0cf089881e444f0e32820b8c58c8ad23b968516b
ali_he96 <alihesarakitrade@gmail.com>
2025-02-02 16:25:03 +0330

    Initial commit

---
+++ src/main/java/com/example/mediclinic/MediClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediClinicApplication.class, args);
+	}
+
+}
