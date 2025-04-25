74d802454c20a1dc5d57fe7bdc588eb0b7f03009
alihesaraki <alihesarakitrade@gmail.com>
2025-04-25 12:19:24 +0200

    new package

--- src/main/java/com/example/mediclinic/repository/MedicineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mediclinic.repository;
-
-import com.example.mediclinic.model.Medicine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicineRepository extends JpaRepository<Medicine, Long> {
-}
--- src/main/java/com/example/mediclinic/repository/SpecializationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mediclinic.repository;
-
-import com.example.mediclinic.model.Specialization;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
-    Optional<Specialization> findByName(String name);
-}
